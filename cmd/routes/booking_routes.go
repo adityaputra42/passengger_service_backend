bdc22f3b4332bbcb42a20d08e0b3c9a4e3d2805a
adityaputra42 <aditiyaputra42@gmail.com>
2026-04-01 16:54:16 +0700

    add booking route

---
+++ cmd/routes/booking_routes.go
@@ -0,0 +1,31 @@
+package routes
+
+import (
+	"passenger_service_backend/internal/handler"
+	"passenger_service_backend/internal/middleware"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func BookingRoutes(r chi.Router, h *handler.BookingHandler, deps Dependencies) {
+	authMiddleware := middleware.AuthMiddleware(deps.UserService, deps.JWTService)
+
+	r.Route("/bookings", func(r chi.Router) {
+		r.Use(authMiddleware)
+		r.Post("/", h.Create)
+		r.Get("/{id}", h.GetByID)
+		r.Get("/locator/{locator}", h.GetByLocator)
+		r.Put("/{id}/contact", h.UpdateContact)
+		r.Delete("/{id}", h.Cancel)
+		r.Post("/passengers/{passengerID}/ssr", h.AddSSR)
+		r.Delete("/ssr/{ssrID}", h.RemoveSSR)
+		r.Post("/passengers/{passengerID}/meal", h.AddMeal)
+		r.Delete("/meal/{mealID}", h.RemoveMeal)
+
+		r.Group(func(r chi.Router) {
+			r.Use(middleware.RequireAdminArea(deps.RBACService))
+			r.Get("/", h.List)
+
+		})
+	})
+}
---
+++ cmd/routes/checkin_routes.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"passenger_service_backend/internal/handler"
+	"passenger_service_backend/internal/middleware"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func CheckInRoutes(r chi.Router, h *handler.CheckinHandler, deps Dependencies) {
+	authMiddleware := middleware.AuthMiddleware(deps.UserService, deps.JWTService)
+
+	r.Route("/checkin", func(r chi.Router) {
+		r.Use(authMiddleware)
+		r.Post("/", h.Checkin)
+		r.Get("/passenger/{passengerID} ", h.GetByPassenger)
+	})
+}
